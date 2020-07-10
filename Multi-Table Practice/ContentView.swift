27fd8e20b0e3ca79eeef4021a7dcd93d60ac8508
Nate Lee <nateleee.dev@gmail.com>
2020-07-10 17:00:14 +0800

    Initial Commit

---
+++ Multi-Table Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Multi-Table Practice
+//
+//  Created by Nate Lee on 7/10/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
