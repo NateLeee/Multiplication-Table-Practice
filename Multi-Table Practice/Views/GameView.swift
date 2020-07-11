8bd5fe7855284392c3b62d73f9c2e006a96ce9a3
Nate Lee <nateleee.dev@gmail.com>
2020-07-11 11:22:51 +0800

    Added another view file.

---
+++ Multi-Table Practice/Views/GameView.swift
@@ -0,0 +1,21 @@
+//
+//  GameView.swift
+//  Multi-Table Practice
+//
+//  Created by Nate Lee on 7/11/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text("This is the game view!")
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
